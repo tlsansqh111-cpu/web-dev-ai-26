e1257c3307bca929ee8c8bb7d15ab8088ba87f0d
tlsansqh111-cpu <tlsansqh111@gmail.com>
2026-02-04 14:38:01 +0900

    s

--- web-dev-ai-26/03-javascript/practice05-function-updown.js
+++ web-dev-ai-26/03-javascript/practice05-function-updown.js
@@ -13,13 +13,14 @@ while (true) {
   count++;
   const a = prompt(`1부터 ${x}까지의 숫자를 입력해주세요`);
   console.log(`입력한 값은 ${a}입니다`);
-  if (isNaN(a)) {
+  if (isnan(a)) {
     const a = prompt("숫자만 입력해주세요");
     count_2++;
   } else if (a === X.toString()) {
     count_3 = count - count_2;
+
     console.log(
       `성공!  잘못입력한 값 ${count_2}번과 ${count_3}번의 시도로 총 ${count}번의 입력끝에 성공하셨습니다`,
     );
-
+}
 }

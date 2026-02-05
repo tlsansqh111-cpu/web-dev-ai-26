6316a393f10fc30465ea209b0a6cb67e834fecf7
tlsansqh111-cpu <tlsansqh111@gmail.com>
2026-02-05 14:47:09 +0900

    d

--- web-dev-ai-26/03-javascript/practice06-object-rps.js
+++ web-dev-ai-26/03-javascript/practice06-object-rps.js
@@ -5,27 +5,32 @@
 
  };
  // 객체 오브젝트 3개 의 수로 Math.random 사용
-const ran = randoms[Math.floor(Math.random() * 3)];
-
- console.log(` ${ran}`);
 
 
  while (true) {
+    const ran = randoms[Math.floor(Math.random() * 3)];
+    console.log(` ${ran}`);
+    const count = {
+        0: 승리,
+        1: 패배,
+        2: 무승부
+    };
+
     let input = prompt("가위/바위/보 중 하나를 입력")
 
     if (input === null) {
         console.log("null");
-        alert(`종료합니다.`)
+        alert(`종료합니다. `)
         break;
     }
 
    let player = input;
    let computer = ran;
- 
+   count++
 
     if (player === ran) {
         console.log("무승부")
-        alert(`무승부.` ) //메세지 띄우기 코드
+        alert(`무승부.` )
         
         } else if (
             (player === "가위" && computer === "보" ) ||

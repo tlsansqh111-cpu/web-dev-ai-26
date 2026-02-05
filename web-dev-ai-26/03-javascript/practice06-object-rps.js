83ca73a45758ad4aefc452daf1ec8fdb821e6246
tlsansqh111-cpu <tlsansqh111@gmail.com>
2026-02-05 13:37:12 +0900

    ee

--- web-dev-ai-26/03-javascript/practice06-object-rps.js
+++ web-dev-ai-26/03-javascript/practice06-object-rps.js
@@ -10,26 +10,33 @@ const ran = randoms[Math.floor(Math.random() * 3)];
 
  while (true) {
     let input = prompt("가위/바위/보 중 하나를 입력")
-    // null 취소 "null" -입력 값 문자- 따옴표 차이 발생
+
     if (input === null) {
         console.log("null");
         alert("종료합니다.")
         break;
     }
-    let userinput = input;
+
+   let player = input;
+   let computer = ran;
 
 
-    if (userinput === ran) {
+    if (player === ran) {
+        console.log("무승부")
         alert(`무승부.` ) //메세지 띄우기 코드
+        break;
         } else if (
-            (userinput === "가위" && ran ==="보" ) ||
-            (userinput === "바위" && ran ==="가위" ) ||
-            (userinput === "보" && ran ==="바위" )
+            (player === "가위" && computer ==="보" ) ||
+            (player === "바위" && computer ==="가위" ) ||
+            (player=== "보" && computer ==="바위" )
         )  {
         console.log("승리.");
         alert("승리.")
+        break;
         } else {
-            console.log(패배)
+            console.log("패배")
+            alert("패배.")
+            break;
     }
 }
         

19f7e5b6fc8f28fca4e2aa1b950d63941fa61132
tlsansqh111-cpu <tlsansqh111@gmail.com>
2026-03-18 18:26:35 +0900

    o

---
+++ web-dev-ai-26/04-mysql/01_DDL.sql
@@ -0,0 +1,10 @@
+/*
+   - 데이터(data) : 화면에 보이거나, 사용자가 입력하거나, 저장해야 하는 정보
+   - 데이터베이스(database) : 데이터를 저장하고 필요할 떄 꺼내 쓰는 공간
+   - DBMS(database management system) : 데이터베이스를 만들고, 저장하고, 수정하고, 조회할 수 있게 하는 프로그램
+      데이터베이스를 관리하는 프로그램
+   - RDBMS(realtional database management system): 관계형 데이터베이스 관리 시스템
+      예) MySQL, Oracle, PostgreSQL
+
+   - 엔티티(Entity)
+*/
